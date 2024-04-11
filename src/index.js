543125eea5876c37ec663770f0a5ac446e2c71d8
Aka, but nice <120267628+akabutnicer@users.noreply.github.com>
2024-04-11 09:08:35 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.body
 );
 
 // If you want to start measuring performance in your app, pass a function
