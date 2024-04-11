c68ed4919c162a4ffb4980d869483c0285b09b8a
Aka, but nice <120267628+akabutnicer@users.noreply.github.com>
2024-04-11 07:57:03 -0400

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./*/**/*.{js,ts,jsx,tsx}",
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./build/**/*.{js,ts,jsx,tsx,html}",
+
+    "./node_modules/flowbite/**/*.js",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        grapenuts: ["Grape Nuts", "cursive"],
+        habibi: ["Habibi", "sans-serif"],
+        roboto: ["Roboto", "sans-serif"],
+        robotoregular: ["RobotoRegular", "sans-serif"],
+        boldroboto: ["RobotoBold", "Noto Sans"],
+        "vsans": ['Vixcord Sans']
+      },
+      transitionProperty: {
+        width: "width",
+      },
+      spacing: {
+        17: "65px",
+      },
+      fontSize: {
+        xxs: "11px",
+      },
+    },
+  },
+  plugins: [require("@tailwindcss/forms"), require("flowbite/plugin")],
+};
+
