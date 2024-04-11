2efe45fb7cc53e6ba635c6ab645966ec24c22c82
Aka, but nice <120267628+akabutnicer@users.noreply.github.com>
2024-04-10 21:19:31 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,15 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { StyledEngineProvider } from '@mui/material/styles';
 
 
 ReactDOM.render(
-  <StyledEngineProvider injectFirst>
   <React.StrictMode>
     <App />
-  </React.StrictMode>
-  </StyledEngineProvider>,
+  </React.StrictMode>,
   document.getElementById('root')
 );
 
