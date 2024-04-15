692d22457dcc2c6f6a24be281216a4c6f60c9b8a
Aka, but nice <120267628+akabutnicer@users.noreply.github.com>
2024-04-15 17:23:09 -0400

    Update supabase.js

--- api/utils/supabase.js
+++ api/utils/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js"
 const supabase = createClient(
-  "https://mltybpooqccvxltvwyzj.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1sdHlicG9vcWNjdnhsdHZ3eXpqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDE4MDA3NDMsImV4cCI6MjAxNzM3Njc0M30.lTBPDWKOOr895-QRvwxFvlNALR_pBIZBfmURiz6HIPU"
+   process.env.SUPABASE_URL,
+   process.env.SUPABASE_ANON_KEY
 )
 
 export default supabase
