07983ef9c9868db8924fffa1b6a0c2b6adeb85ce
Aka, but nice <120267628+akabutnicer@users.noreply.github.com>
2024-04-16 09:51:45 -0400

    Update supabase.js

--- src/utils/supabase.js
+++ src/utils/supabase.js
@@ -1,4 +1,5 @@
-import { createClient } from "@supabase/supabase-js"
+import { createClient } from "@supabase/supabase-js";
+console.log(process.env);
 const supabase = createClient(
    process.env.REACT_APP_SUPABASE_URL,
    process.env.REACT_APP_SUPABASE_ANON_KEY
