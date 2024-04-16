976fc0208c95b9084a08671a8fcb6f6b636f0419
Aka, but nice <120267628+akabutnicer@users.noreply.github.com>
2024-04-16 09:52:37 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+console.log(process.env);
 
 ReactDOM.render(
   <React.StrictMode>
