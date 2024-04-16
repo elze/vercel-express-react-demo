098e3a64c52f53660683956b06f92b34a6ec188f
Aka, but nice <120267628+akabutnicer@users.noreply.github.com>
2024-04-16 09:13:57 -0400

    Update supabase.js

--- src/utils/supabase.js
+++ src/utils/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js"
 const supabase = createClient(
-   process.env.SUPABASE_URL,
-   process.env.SUPABASE_ANON_KEY
+   process.env.REACT_APP_SUPABASE_URL,
+   process.env.REACT_APP_SUPABASE_ANON_KEY
 )
 
 export default supabase
